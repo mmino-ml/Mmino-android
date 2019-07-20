08d1637e818928f4df0ee80fcdcb2e6bb15ca523
nyoniempire <brucejnyoni@gmail.com>
2019-07-20 15:51:45 +0200

    First build
    This is a first build.

---
+++ app/src/main/java/com/anesu/mmino/View/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.anesu.mmino.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.anesu.mmino.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
